9e9c7915d5447fee1a613548dab64c43fe42e11a
Richard Ziupsnys <64844585+Richy-Z@users.noreply.github.com>
2026-02-20 15:51:38 +0000

    Formatting

--- src/assets/js/ThemeSync.js
+++ src/assets/js/ThemeSync.js
@@ -3,42 +3,42 @@
 // apart from the favicon I think, which still requires JS
 
 function updateAds(theme) {
-    document.querySelectorAll(".sponsored-image").forEach(img => {
-        img.src = `/assets/ads/voxlis.${theme}.big.png`;
-    });
+	document.querySelectorAll(".sponsored-image").forEach(img => {
+		img.src = `/assets/ads/voxlis.${theme}.big.png`;
+	});
 }
 
 function updateLogos(theme) {
-    document.querySelectorAll(".voxlis-logo").forEach(img => {
-        img.src = `/assets/branding/voxlis.${theme}.png`;
-    });
+	document.querySelectorAll(".voxlis-logo").forEach(img => {
+		img.src = `/assets/branding/voxlis.${theme}.png`;
+	});
 }
 
 function updateHearts(theme) {
-    const src = `/assets/img/hearts/heart.${theme}.svg`;
-    document.querySelectorAll(".heart-img").forEach(img => {
-        img.src = src;
-        img.style.backgroundImage = `url(${src})`
-    });
+	const src = `/assets/img/hearts/heart.${theme}.svg`;
+	document.querySelectorAll(".heart-img").forEach(img => {
+		img.src = src;
+		img.style.backgroundImage = `url(${src})`
+	});
 }
 
 const favicon = document.querySelector("link[rel~='icon']");
 function updatePageIcon(theme) {
-    favicon.href = `/assets/img/hearts/heart.${theme}.svg`;
+	favicon.href = `/assets/img/hearts/heart.${theme}.svg`;
 }
 
 function updateAll() {
-    const theme = document.documentElement.dataset.theme || "red";
-    updateAds(theme);
-    updateLogos(theme);
-    updateHearts(theme);
-    updatePageIcon(theme);
+	const theme = document.documentElement.dataset.theme || "red";
+	updateAds(theme);
+	updateLogos(theme);
+	updateHearts(theme);
+	updatePageIcon(theme);
 }
 
 document.addEventListener("DOMContentLoaded", updateAll);
 
 const observer = new MutationObserver(updateAll);
 observer.observe(document.documentElement, {
-    attributes: true,
-    attributeFilter: ["data-theme"],
+	attributes: true,
+	attributeFilter: ["data-theme"],
 });
